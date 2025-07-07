9a743e915a6eeeaa6c55bc03b8e0862b7eb66da8
DevSysEngineer <info@kvansteijn.nl>
2025-07-07 22:07:05 +0200

    Improved moni

--- naemon/manifests/init.pp
+++ naemon/manifests/init.pp
@@ -1,6 +1,22 @@
 class naemon (
-
+  Optional[String]  $webserver_uid  = undef,
+  Optional[String]  $webserver_gid  = undef
 ) {
+  # Try to get uid and gid
+  if ($webserver_uid == undef or $webserver_gid == undef) {
+    if (defined(Class['nginx'])) {
+      $webserver_uid_correct = $nginx::run_user
+      $webserver_gid_correct = $nginx::run_group
+    } else {
+      $webserver_uid_correct = undef
+      $webserver_gid_correct = undef
+    }
+  } else {
+    $webserver_uid_correct = $webserver_uid
+    $webserver_gid_correct = $webserver_gid
+  }
+
+  # Install package
   package { 'naemon':
     ensure          => installed,
     install_options => ['--no-install-recommends', '--no-install-suggests'],
