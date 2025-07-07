56e5f8a3bcf58b8a202e9af451e475ee5a08957d
DevSysEngineer <info@kvansteijn.nl>
2025-07-07 17:05:17 +0200

    Improved check

--- ncpa/manifests/mirth_connect.pp
+++ ncpa/manifests/mirth_connect.pp
@@ -2,10 +2,11 @@ class ncpa::mirth_connect (
   Enum['present','absent']  $ensure     = present,
   Optional[String]          $package    = undef
 ) {
+  $systemd_enable = $ncpa::systemd_enable
   basic_settings::monitoring_custom { 'mirth_connect':
     ensure   => $ensure,
     package  => $package,
     friendly => 'Mirth Connect',
-    source   => 'puppet:///modules/ncpa/check_mirth_conect',
+    content  => template('mysql/check_mysql'),
   }
 }
