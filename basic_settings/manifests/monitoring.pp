c7cd304f77a8095bc8292bc009071d4d089cb024
DevSysEngineer <info@kvansteijn.nl>
2025-07-25 13:10:32 +0200

    Update monitoring.pp

--- basic_settings/manifests/monitoring.pp
+++ basic_settings/manifests/monitoring.pp
@@ -1,9 +1,9 @@
 class basic_settings::monitoring (
-  String              $mail_to          = 'root',
-  String              $mail_package     = 'postfix',
-  Enum['none','ncpa'] $package          = 'none',
-  Boolean             $package_install  = false,
-  String              $server_fdqn      = $facts['networking']['fqdn']
+  String                        $mail_to          = 'root',
+  String                        $mail_package     = 'postfix',
+  Enum['none','openitcockpit']  $package          = 'none',
+  Boolean                       $package_install  = false,
+  String                        $server_fdqn      = $facts['networking']['fqdn']
 ) {
   # Install package
   package { [$mail_package, 'mailutils']:
