97eefeece7b194090b6ee10f7fa01803ebdf2226
DevSysEngineer <info@kvansteijn.nl>
2025-07-28 22:47:43 +0200

    Improved mirth conenct

--- openitcockpit/manifests/agent_mirth_connect.pp
+++ openitcockpit/manifests/agent_mirth_connect.pp
@@ -2,6 +2,7 @@ class openitcockpit::agent_mirth_connect (
   Enum['present','absent']  $ensure     = present,
   Optional[String]          $package    = undef
 ) {
+  $systemd_enable = defined(Package['systemd'])
   basic_settings::monitoring_custom { 'mirth_connect':
     ensure   => $ensure,
     package  => $package,
