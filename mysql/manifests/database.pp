5095c04573f558719b79bd02095814985bb10e61
DevSysEngineer <info@kvansteijn.nl>
2025-08-09 13:32:00 +0200

    Added import option

--- mysql/manifests/database.pp
+++ mysql/manifests/database.pp
@@ -1,8 +1,9 @@
 define mysql::database (
   Enum['present','absent']    $ensure,
-  String                      $charset = 'utf8',
-  String                      $collate = 'utf8_general_ci',
-  Boolean                     $destroy = false
+  String                      $charset  = 'utf8',
+  String                      $collate  = 'utf8_general_ci',
+  Boolean                     $destroy  = false,
+  Optional[String]            $import   = undef,
 ) {
   # Set requirements
   Exec {
@@ -12,9 +13,23 @@ define mysql::database (
   # Run query
   case $ensure {
     'present': {
+      # Check if we need import SQL to database
+      if ($import != undef) {
+        # Import database from file
+        exec { "mysql_database_import_${title}":
+          command     => "/usr/bin/bash -c \"mysql --defaults-file=${mysql::defaults_file} -D ${title} ' < ${import}",
+          refreshonly => true,
+        }
+        $notify = Excec["mysql_database_import_${title}"]
+      } else {
+        $notify = undef
+      }
+
+      # Create database
       exec { "mysql_create_database_${title}":
         unless  => "/usr/bin/bash -c \"mysql --defaults-file=${mysql::defaults_file} -NBe 'SHOW DATABASES;' | grep -qx '${title}'\"",
         command => "mysql --defaults-file=${mysql::defaults_file} -e \"CREATE DATABASE \\`${title}\\` DEFAULT CHARACTER SET = '${charset}' DEFAULT COLLATE = '${collate}';\"", #lint:ignore:140chars
+        notify  => $notify,
       }
     }
     'absent': {
