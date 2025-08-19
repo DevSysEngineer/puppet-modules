e9109b69c113e10192544ff4e71ef82fcad0fcb4
DevSysEngineer <info@kvansteijn.nl>
2025-08-19 17:39:48 +0200

    Update timezone.pp

--- basic_settings/manifests/timezone.pp
+++ basic_settings/manifests/timezone.pp
@@ -49,7 +49,7 @@ class basic_settings::timezone (
       content => template('basic_settings/systemd/timesyncd.conf'),
       owner   => 'root',
       group   => 'root',
-      mode    => '0644',
+      mode    => '0644', # Important
       notify  => Exec['systemd_timezone_daemon_reload'],
       require => Package['systemd-timesyncd'],
     }
