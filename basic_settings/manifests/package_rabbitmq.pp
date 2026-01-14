334237b98799ea3da67c309e7ccb3fb16acd34c7
DevSysEngineer <info@kvansteijn.nl>
2026-01-14 10:39:35 +0100

    Improved RabbitMQ

--- basic_settings/manifests/package_rabbitmq.pp
+++ basic_settings/manifests/package_rabbitmq.pp
@@ -20,23 +20,23 @@ class basic_settings::package_rabbitmq (
   if ($enable) {
     # Get source
     if ($deb_version == '822') {
-      $source_erlang  = "Types: deb\nURIs: https://dl.cloudsmith.io/public/rabbitmq/rabbitmq-erlang/deb/${os_parent}\nSuites: ${os_name}\nComponents: main\nSigned-By:${key_erlang}\n"
-      $source_server  = "Types: deb\nURIs: https://dl.cloudsmith.io/public/rabbitmq/rabbitmq-server/deb/${os_parent}\nSuites: ${os_name}\nComponents: main\nSigned-By:${key_server}\n"
+      $source_erlang  = "Types: deb\nURIs: https://deb1.rabbitmq.com/rabbitmq-erlang/${os_parent}/${os_name}\nSuites: ${os_name}\nComponents: main\nSigned-By:${key_erlang}\n"
+      $source_server  = "Types: deb\nURIs: https://deb1.rabbitmq.com/rabbitmq-server/${os_parent}/${os_name}\nSuites: ${os_name}\nComponents: main\nSigned-By:${key_server}\n"
     } else {
-      $source_erlang = "deb [signed-by=${key_erlang}] https://dl.cloudsmith.io/public/rabbitmq/rabbitmq-erlang/deb/${os_parent} ${os_name} main\n"
-      $source_server = "deb [signed-by=${key_server}] https://dl.cloudsmith.io/public/rabbitmq/rabbitmq-server/deb/${os_parent} ${os_name} main\n"
+      $source_erlang = "deb [signed-by=${key_erlang}] https://deb1.rabbitmq.com/rabbitmq-erlang/${os_parent}/${os_name} ${os_name} main\n"
+      $source_server = "deb [signed-by=${key_server}] https://deb1.rabbitmq.com/rabbitmq-server/${os_parent}/${os_name} ${os_name} main\n"
     }
 
     # Install Rabbitmq erlang repo
     exec { 'package_rabbitmq_erlang_source':
-      command => "/usr/bin/printf \"# Managed by puppet\n${source_erlang}\" > ${file_erlang}; /usr/bin/curl -fsSL https://dl.cloudsmith.io/public/rabbitmq/rabbitmq-erlang/gpg.E495BB49CC4BBE5B.key | gpg --dearmor | tee ${key_erlang} >/dev/null; chmod 644 ${key_erlang}; /usr/bin/apt-get update",
+      command => "/usr/bin/printf \"# Managed by puppet\n${source_erlang}\" > ${file_erlang}; /usr/bin/curl -fsSL https://keys.openpgp.org/vks/v1/by-fingerprint/0A9AF2115F4687BD29803A206B73A36E6026DFCA | gpg --dearmor | tee ${key_erlang} >/dev/null; chmod 644 ${key_erlang}; /usr/bin/apt-get update",
       unless  => "[ -e ${file_erlang} ]",
       require => Package['apt', 'apt-transport-https', 'curl', 'gnupg'],
     }
 
     # Install Rabbitmq server repo
     exec { 'package_rabbitmq_server_source':
-      command => "/usr/bin/printf \"# Managed by puppet\n${source_server}\" >  ${file_server}; /usr/bin/curl -fsSL https://dl.cloudsmith.io/public/rabbitmq/rabbitmq-server/gpg.9F4587F226208342.key | gpg --dearmor | tee ${key_server} >/dev/null; chmod 644 ${key_server}; /usr/bin/apt-get update",
+      command => "/usr/bin/printf \"# Managed by puppet\n${source_server}\" >  ${file_server}; /usr/bin/curl -fsSL https://keys.openpgp.org/vks/v1/by-fingerprint/0A9AF2115F4687BD29803A206B73A36E6026DFCA | gpg --dearmor | tee ${key_server} >/dev/null; chmod 644 ${key_server}; /usr/bin/apt-get update",
       unless  => "[ -e  ${file_server} ]",
       require => Package['apt', 'apt-transport-https', 'curl', 'gnupg'],
     }
