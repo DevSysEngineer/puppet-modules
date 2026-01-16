f356fdfc7d6b44633e1c796405b2dfe194b47e58
DevSysEngineer <info@kvansteijn.nl>
2026-01-16 12:01:15 +0100

    Improved service script

--- basic_settings/manifests/systemd_service.pp
+++ basic_settings/manifests/systemd_service.pp
@@ -1,15 +1,17 @@
 define basic_settings::systemd_service (
   String                    $description,
-  String                    $daemon_reload        = 'systemd_daemon_reload',
-  Boolean                   $enable               = true,
-  Enum['present','absent']  $ensure               = present,
-  Hash                      $install              = {
+  String                    $daemon_reload              = 'systemd_daemon_reload',
+  Boolean                   $enable                     = true,
+  Enum['present','absent']  $ensure                     = present,
+  Hash                      $install                    = {
     'WantedBy'  => 'multi-user.target',
   },
-  Optional[Boolean]         $monitoring_enable    = undef,
-  Optional[String]          $monitoring_package   = undef,
-  Hash                      $service              = {},
-  Hash                      $unit                 = {},
+  Optional[Boolean]         $monitoring_enable          = undef,
+  Optional[String]          $monitoring_package         = undef,
+  Optional[String]          $monitoring_active_windows  = undef,
+  Optional[String]          $monitoring_active_days     = undef,
+  Hash                      $service                    = {},
+  Hash                      $unit                       = {},
 ) {
   # Check if systemd package is not defined
   if (!defined(Package['systemd'])) {
@@ -42,14 +44,18 @@ define basic_settings::systemd_service (
     if ($monitoring_enable != undef and $monitoring_package != 'none') {
       $monitoring_ensure = $monitoring_enable ? { true => 'present', default => absent }
       basic_settings::monitoring_service { $title:
-        ensure  => $monitoring_ensure,
-        package => $monitoring_package,
+        ensure         => $monitoring_ensure,
+        package        => $monitoring_package,
+        active_windows => $monitoring_active_windows,
+        active_days    => $monitoring_active_days,
       }
     }
   } elsif ($monitoring_package != 'none') {
     basic_settings::monitoring_service { $title:
-      ensure  => $ensure,
-      package => $monitoring_package,
+      ensure         => $monitoring_ensure,
+      package        => $monitoring_package,
+      active_windows => $monitoring_active_windows,
+      active_days    => $monitoring_active_days,
     }
   }
 }
